04b536951c8b800f96f6c6a2db9ede748b46291a
Keren-Happuc Fleurimond <keren@noukod.com>
2018-11-26 21:08:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,8 +10,8 @@ INSERT INTO authors (name) VALUES ("Arthur Miller");
 INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
 INSERT INTO books (title, year, series_id) VALUES ("The Odyssey", 1954, 1);
 INSERT INTO books (title, year, series_id) VALUES ("Pygmalion", 1954, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
-INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Life", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Freedom", 1998, 2);
 INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
 
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 2, 2);
